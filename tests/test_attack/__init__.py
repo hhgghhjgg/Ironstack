7b4b6f4a1a0b2bd68a132551353ad82d14b71b2a
Didio_ms <mhmdamas582@gmail.com>
2026-05-22 03:00:49 +0330

    ایجاد فایل

---
+++ tests/test_attack/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+IronStack Test Suite - Attack Layer
+====================================
+Unit and integration tests for IronStack attack modules:
+
+- test_scanner.py      : Vulnerability scanner tests
+- test_hooker.py       : Dynamic instrumentation (Frida) tests
+- test_disassembler.py : Binary disassembler (Capstone) tests
+- test_emulator.py     : CPU emulator (Unicorn) tests
+"""
